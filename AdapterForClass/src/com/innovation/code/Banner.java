4bf5941e68434e6b6bea275123be9a6e7b87add9
UbiQwQ <lt1550919167@163.com>
2018-05-05 17:29:53 +0800

    Adapter DesignPattern For Class

---
+++ AdapterForClass/src/com/innovation/code/Banner.java
@@ -0,0 +1,20 @@
+package com.innovation.code;
+/*
+ * @author Innovation
+ */
+public class Banner {
+	private String string;
+
+	public Banner(String string) {
+		super();
+		this.string = string;
+	};
+	
+	public void showWithParen(){
+		System.out.println("(" + string + ")");
+	}
+	
+	public void showWithAster(){
+		System.out.println("*" + string + "*");
+	}
+}
---
+++ AdapterForClass/src/com/innovation/code/Print.java
@@ -0,0 +1,8 @@
+package com.innovation.code;
+/*
+ * @author Innovation
+ */
+public interface Print {
+	public abstract void printWeak();
+	public abstract void printStrong();
+}

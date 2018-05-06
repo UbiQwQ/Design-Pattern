f7838762e3f0c10104f6014e01404806d8e7d75d
UbiQwQ <lt1550919167@163.com>
2018-05-06 17:18:51 +0800

    Template Method

---
+++ Template/src/com/innovation/code/CharDisplay.java
@@ -0,0 +1,20 @@
+package com.innovation.code;
+
+public class CharDisplay extends AbstractDisplay {
+	private char ch;
+	public CharDisplay(char ch){
+		this.ch = ch;
+	}
+	@Override
+	public void open() {
+		System.out.print("<<");
+	}
+	@Override
+	public void print() {
+		System.out.print(ch);
+	}
+	@Override
+	public void close() {
+		System.out.println(">>");
+	}
+}
